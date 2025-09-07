0020c4a18ad5ed1d22189cad35676ffe7e230914
DevLouix <94640024+DevLouix@users.noreply.github.com>
2025-09-07 05:48:47 +0000

    v2 init

---
+++ v2/src/app/sitemap.ts
@@ -0,0 +1,24 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://devlouix.com",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://blog.devlouix.com",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    // {
+    // //   url: 'https://devlouix.com/blog',
+    // //   lastModified: new Date(),
+    // //   changeFrequency: 'weekly',
+    // //   priority: 0.5,
+    // }
+  ];
+}
