004ea553234a9a274f38134d6f15100280020613
Dev Louix <94640024+DevLouix@users.noreply.github.com>
2026-04-16 19:21:19 +0000

    v3 init

--- v3/src/app/(frontend)/layout.tsx
+++ v3/src/app/(frontend)/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './styles.css'
+// import './styles.css'
 import { ModalProvider } from '@/providers/ModalProvider'
 import { GlobalModalManager } from '@/components/modals/GlobalModalManager'
 import { getPayload } from 'payload'
--- v3/src/payload.config.ts
+++ v3/src/payload.config.ts
@@ -33,7 +33,7 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
     meta: {
-      titleSuffix: ' - Blog',
+      titleSuffix: ' - Devlouix',
     },
   },
   maxDepth: 5, // Global maximum allowed (safety net)
